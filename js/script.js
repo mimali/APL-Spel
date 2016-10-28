2e135cd269a356d66d33afe58aa0668db4af109c
HannesDB <hannes_dernbrant@otmail.com>
2016-10-28 13:33:38 +0200

    bugfix quizgame

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
         var chosenAnswer;
         // All user answers in array form
         var allUserAnswers = [];
-        //All correct answers
+        //All correct answers for comparison to user's answers
         var correctAnswers = ["option1.1", "option2.1", "option3.1", "option4.1"];
         // And then in booleans
         var boolCorrectAnswers = [];
@@ -19,14 +19,14 @@
         // When clicking button...
         $(".quizButton").click(function(){
             // Add +1 step in process
-            currentStep++
+            currentStep++;
             // If quiz is over -> compare user answers with correct answers
             if (currentStep > 4) {
-                compareAnswers()
+                compareAnswers();
             };
 
             // Hide all irrelevant steps, show active
-            getValidStep()
+            getValidStep();
               
         });
 
@@ -40,6 +40,7 @@
             //Change all other options to unmarked class 
             var siblings = $(this).siblings();
             var otherOptions = siblings.slice(0,4);
+
             for (var i = 0; i < otherOptions.length; i++) {
                 $(otherOptions[i]).switchClass("selectedOption", "unselectedOption");
             };
@@ -51,13 +52,18 @@
         
         function getValidStep(){
             console.log("currentStep: " + currentStep);
-            // Loop through all steps in quiz, show only current
+            // Loop through all steps in quiz...
             for (var i = 0; i < stepsInQuiz.length; i++) {
-                if (i != currentStep) {
-                    $(stepsInQuiz[i]).hide("slide", {direction : "left"});
-                } else {
-                    $(stepsInQuiz[i]).show("slide", {direction : "right"}); 
-                };
+                // Check what step the quiz is on, show that step and hide previous
+                if (i == currentStep && currentStep > 0) {
+                    $(stepsInQuiz[i-1]).hide("slide", {direction : "left"});
+                    $(stepsInQuiz[i]).show("slide", {direction : "right"});
+                }
+                // Show without animation on Start stage
+                else if (currentStep == 0) {
+                    $(stepsInQuiz[0]).show();
+                }; 
+                     
             };
         };
 
